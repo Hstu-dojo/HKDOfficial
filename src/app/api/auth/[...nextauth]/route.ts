29cc97bc2682864a846a4b88b5e0d1728f1755e8
Mr. Hasan <hasanshahriar32@gmail.com>
2024-01-29 16:24:32 +0600

    update

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,20 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+export const authOptions = {
+    // Configure one or more authentication providers
+    adapter: PrismaAdapter(prisma),
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_ID || "",
+            clientSecret: process.env.GOOGLE_SECRET || "",
+        }),
+        // ...add more providers here
+    ],
+    
+};
+
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST };
