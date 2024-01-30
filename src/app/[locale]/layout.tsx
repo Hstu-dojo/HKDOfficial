a7319654e75d9f8250f65757daae1511ed9f8245
Mr. Hasan <hasanshahriar32@gmail.com>
2024-01-31 02:25:42 +0600

    updated locales

---
+++ src/app/[locale]/layout.tsx
@@ -0,0 +1,79 @@
+import "../globals.css";
+import type { Metadata } from "next";
+import { Roboto } from "next/font/google";
+import { Toaster } from "sonner";
+import { siteConfig } from "@/config/site";
+import BackToTop from "@/components/back-to-top";
+import { getServerSession } from "next-auth";
+import SessionProvider from "@/utils/SessionProvider";
+
+const roboto = Roboto({
+  subsets: ["latin"],
+  weight: ["400", "500", "700"],
+  display: "swap",
+});
+
+export const metadata: Metadata = {
+  metadataBase: new URL(process.env.NEXT_PUBLIC_APP_URL!),
+  applicationName: siteConfig.name,
+  title: {
+    default: siteConfig.name,
+    template: `%s - ${siteConfig.name}`,
+  },
+  description: siteConfig.description,
+  manifest: "/manifest.json",
+  keywords: siteConfig.keywords,
+  authors: [
+    {
+      name: "Shahriar Hasan",
+      url: "https://shahriarhasan.vercel.app",
+    },
+  ],
+  creator: "Shahriar Hasan",
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: siteConfig.name,
+    // startUpImage: [],
+  },
+  formatDetection: {
+    telephone: false,
+  },
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: siteConfig.url,
+    title: siteConfig.name,
+    description: siteConfig.description,
+    siteName: siteConfig.name,
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: siteConfig.name,
+    description: siteConfig.description,
+    images: [`${siteConfig.url}/og-image.jpg`],
+    creator: "shahriar hasan",
+  },
+  icons: {
+    icon: "/favicon.ico",
+  },
+};
+
+export default async function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const session = await getServerSession();
+  return (
+    <body
+      className={`${roboto.className} dark:bg-slate-850 dark:text-slate-400`}
+    >
+      
+        <SessionProvider session={session}>{children}</SessionProvider>
+
+      <Toaster richColors />
+      <BackToTop />
+    </body>
+  );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,81 +1,37 @@
-import "./globals.css";
-import type { Metadata } from "next";
-import { Roboto } from "next/font/google";
+import { Inter } from "next/font/google";
+import localFont from "next/font/local";
+import { cookies } from "next/headers";
+import { cn } from "@/lib/utils";
 import { ThemeProvider } from "@/context/ThemeProvider";
-import { Toaster } from "sonner";
-import { siteConfig } from "@/config/site";
-import BackToTop from "@/components/back-to-top";
-import { getServerSession } from "next-auth";
-import SessionProvider from "@/utils/SessionProvider";
-
-const roboto = Roboto({
+import "./globals.css";
+export const fontSans = Inter({
   subsets: ["latin"],
-  weight: ["400", "500", "700"],
-  display: "swap",
+  variable: "--font-sans",
 });
 
-export const metadata: Metadata = {
-  metadataBase: new URL(process.env.NEXT_PUBLIC_APP_URL!),
-  applicationName: siteConfig.name,
-  title: {
-    default: siteConfig.name,
-    template: `%s - ${siteConfig.name}`,
-  },
-  description: siteConfig.description,
-  manifest: "/manifest.json",
-  keywords: siteConfig.keywords,
-  authors: [
-    {
-      name: "Shahriar Hasan",
-      url: "https://shahriarhasan.vercel.app",
-    },
-  ],
-  creator: "Shahriar Hasan",
-  appleWebApp: {
-    capable: true,
-    statusBarStyle: "default",
-    title: siteConfig.name,
-    // startUpImage: [],
-  },
-  formatDetection: {
-    telephone: false,
-  },
-  openGraph: {
-    type: "website",
-    locale: "en_US",
-    url: siteConfig.url,
-    title: siteConfig.name,
-    description: siteConfig.description,
-    siteName: siteConfig.name,
-  },
-  twitter: {
-    card: "summary_large_image",
-    title: siteConfig.name,
-    description: siteConfig.description,
-    images: [`${siteConfig.url}/og-image.jpg`],
-    creator: "shahriar hasan",
-  },
-  icons: {
-    icon: "/favicon.ico",
-  },
-};
+const fontHeading = localFont({
+  src: "../assets/fonts/CalSans-SemiBold.woff2",
+  variable: "--font-heading",
+});
 
-export default async function RootLayout({
+export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const session = await getServerSession();
+  const locale = cookies().get("Next-Locale")?.value || "en";
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang={locale} suppressHydrationWarning>
       <body
-        className={`${roboto.className} dark:bg-slate-850 dark:text-slate-400`}
+        className={cn(
+          "font-sans antialiased",
+          fontSans.variable,
+          fontHeading.variable,
+        )}
       >
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-          <SessionProvider session={session}>{children}</SessionProvider>
+          {children}
         </ThemeProvider>
-        <Toaster richColors />
-        <BackToTop />
       </body>
     </html>
   );
