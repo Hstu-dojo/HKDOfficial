28bfa421d94a22ae94b1dc27959d2696632da67c
Mr. Hasan <hasanshahriar32@gmail.com>
2024-02-03 20:35:16 +0600

    update

--- prisma/migrations/20240130175656_image/migration.sql
+++
@@ -1,6 +0,0 @@
--- AlterTable
-ALTER TABLE "user" ALTER COLUMN "emailVerified" SET DEFAULT false;
-ALTER TABLE "user" ALTER COLUMN "image" SET DEFAULT 'https://api.dicebear.com/7.x/lorelei/svg';
-
--- CreateIndex
-CREATE INDEX "email" ON "user"("email");
