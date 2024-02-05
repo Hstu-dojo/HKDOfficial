93ba583c5a68c244c7d734f8b1f468a3af1e81e8
Mr. Hasan <hasanshahriar32@gmail.com>
2024-02-05 15:23:15 +0600

    updae

---
+++ src/app/[locale]/(pages)/onboarding/verify-email/page.tsx
@@ -0,0 +1,12 @@
+import { VerifyMail } from '@/components/component/verify-mail';
+import React from 'react';
+
+const VerifyYourMail = () => {
+    return (
+        <div>
+            <VerifyMail />
+        </div>
+    );
+};
+
+export default VerifyYourMail;
