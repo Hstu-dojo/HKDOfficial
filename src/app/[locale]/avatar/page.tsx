666cb450c32b7e7f2f6442d0c124a765c7ff02f8
Mr. Hasan <hasanshahriar32@gmail.com>
2024-02-07 15:07:23 +0600

    update

---
+++ src/app/[locale]/avatar/page.tsx
@@ -0,0 +1,11 @@
+import { Avatarz } from "@/components/component/avatarz";
+
+const AvatarPage: React.FC = () => {
+  return (
+    <div>
+      <Avatarz />
+    </div>
+  );
+};
+
+export default AvatarPage;
