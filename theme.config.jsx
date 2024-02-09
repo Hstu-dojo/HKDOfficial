dd15e7dea5dec7461d4427f69f1314a098f39a80
Mr. Hasan <hasanshahriar32@gmail.com>
2024-02-10 01:58:56 +0600

    update

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,5 +1,23 @@
 export default {
-  logo: <span>HKD</span>,
+  logo: (
+    <h4
+      style={{
+        display: "flex",
+        flexDir: "row",
+        justifyContent: "",
+        alignItems: "center",
+      }}
+    >
+      <img
+        style={{ width: "20px", height: "20px", marginRight: "8px" }}
+        src="/logo.png"
+      />
+      <strong>HKD</strong>
+    </h4>
+  ),
+  footer: {
+    text: <span>HKD {new Date().getFullYear()} © HSTU KARATE DOJO.</span>,
+  },
   project: {
     link: "https://github.com/hasanshahriar32",
   },
