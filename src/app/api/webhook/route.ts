f28e339d7dba85f275cad8fc3ae7d15c1b642fc6
Mr. Hasan <hasanshahriar32@gmail.com>
2024-02-20 15:25:43 +0600

    update

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -4,9 +4,8 @@ import { prisma } from "@/lib/connect-db";
 export async function POST(req: NextRequest, res: NextResponse) {
   const data = await req.json();
   const result = prisma?.emailLog.create({
-    data: {
-      payload: data,
-    },
+    //@ts-ignore
+    payload: data || {},
   });
   console.log(data);
   return NextResponse.json({ status: 200, statusText: JSON.stringify(result) });
