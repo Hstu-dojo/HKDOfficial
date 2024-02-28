c825db0855db4b7ef2186c53ad017af04361c53b
Mr. Hasan <hasanshahriar32@gmail.com>
2024-02-29 01:16:46 +0600

    update blog section

--- sanity/lib/store.ts
+++ sanity/lib/store.ts
@@ -5,6 +5,6 @@ import * as queryStore from "@sanity/react-loader";
 import { client } from "./client";
 import { token } from "./token";
 
-queryStore.setServerClient(client.withConfig({ token }));
+// queryStore.setServerClient(client.withConfig({ token }));
 
 export const { loadQuery } = queryStore;
