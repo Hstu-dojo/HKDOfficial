71a5d5ec6e52e7b315e638c8f7ba2b6ca29a0716
Mr. Hasan <hasanshahriar32@gmail.com>
2024-02-29 15:42:30 +0600

    blog navbar update

--- src/components/blogs/global/Navbar/NavbarLayout.tsx
+++ src/components/blogs/global/Navbar/NavbarLayout.tsx
@@ -12,7 +12,7 @@ export default function Navbar(props: NavbarProps) {
   const { data } = props;
   const menuItems = data?.menuItems || ([] as MenuItem[]);
   return (
-    <div className="sticky top-0 z-10 flex flex-wrap items-center gap-x-5 bg-white/80 px-4 py-4 backdrop-blur md:px-16 md:py-5 lg:px-32">
+    <div className="sticky w-full top-0 z-10 flex flex-wrap items-center gap-x-5 bg-white/80 px-4 py-4 backdrop-blur md:px-16 md:py-5 lg:px-32">
       {menuItems &&
         menuItems.map((menuItem, key) => {
           const href = resolveHref(menuItem?._type, menuItem?.slug);
@@ -33,6 +33,12 @@ export default function Navbar(props: NavbarProps) {
             </Link>
           );
         })}
+        <Link
+              className={`text-lg hover:text-black font-semibold self-end md:text-xl `}
+              href={"/"}
+            >
+              Home
+            </Link>
     </div>
   );
 }
