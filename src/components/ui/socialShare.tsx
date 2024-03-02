c93b30aaacf0915d92b057331eb8c2afb2403cb6
Mr. Hasan <hasanshahriar32@gmail.com>
2024-03-03 01:09:24 +0600

    shared link add

--- src/components/ui/socialShare.tsx
+++ src/components/ui/socialShare.tsx
@@ -23,6 +23,14 @@ export default function SocialShare() {
     // alert("Link copied to clipboard!");
   };
 
+  const handleSocialShare = (socialPlatform: any) => {
+    // Replace this with your custom share functionality
+    navigator?.share({
+      title: "HKD Blog",
+      text: currentURL,
+    });
+  };
+
   const handleSocialClick = (socialPlatform: any) => {
     // Construct shareable link based on the social platform
     let shareableLink = "";
@@ -90,7 +98,7 @@ export default function SocialShare() {
         </div>
 
         {/* INSTAGRAM ICON */}
-        <div
+        {/* <div
           onClick={() => handleSocialClick("instagram")}
           className="shadow-xl flex h-12 w-12 cursor-pointer items-center justify-center rounded-full border border-pink-200 fill-[#bc2a8d] hover:bg-[#bc2a8d] hover:fill-white hover:shadow-pink-500/50"
         >
@@ -104,7 +112,7 @@ export default function SocialShare() {
             <circle cx="16.806" cy="7.207" r="1.078"></circle>
             <path d="M20.533 6.111A4.605 4.605 0 0 0 17.9 3.479a6.606 6.606 0 0 0-2.186-.42c-.963-.042-1.268-.054-3.71-.054s-2.755 0-3.71.054a6.554 6.554 0 0 0-2.184.42 4.6 4.6 0 0 0-2.633 2.632 6.585 6.585 0 0 0-.419 2.186c-.043.962-.056.267-.056 3.71 0 2.442 0 2.753.056 3.71.015.748.156 1.486.419 2.187a4.61 4.61 0 0 0 2.634 2.632 6.584 6.584 0 0 0 2.185.45c.963.042 1.268.055 3.71.055s2.755 0 3.71-.055a6.615 6.615 0 0 0 2.186-.419 4.613 4.613 0 0 0 2.633-2.633c.263-.7.404-1.438.419-2.186.043-.962.056-1.267.056-3.71s0-2.753-.056-3.71a6.581 6.581 0 0 0-.421-2.217zm-1.218 9.532a5.043 5.043 0 0 1-.311 1.688 2.987 2.987 0 0 1-1.712 1.711 4.985 4.985 0 0 1-1.67.311c-.95.044-1.218.055-3.654.055-2.438 0-2.687 0-3.655-.055a4.96 4.96 0 0 1-1.669-.311 2.985 2.985 0 0 1-1.719-1.711 5.08 5.08 0 0 1-.311-1.669c-.043-.95-.053-1.218-.053-3.654 0-2.437 0-2.686.053-3.655a5.038 5.038 0 0 1 .311-1.687c.305-.789.93-1.41 1.719-1.712a5.01 5.01 0 0 1 1.669-.311c.951-.043 1.218-.055 3.655-.055s2.687 0 3.654.055a4.96 4.96 0 0 1 1.67.311 2.991 2.991 0 0 1 1.712 1.712 5.08 5.08 0 0 1 .311 1.669c.043.951.054 1.218.054 3.655 0 2.436 0 2.698-.043 3.654h-.011z"></path>
           </svg>
-        </div>
+        </div> */}
 
         {/* WHATSAPP ICON */}
         <div
@@ -137,6 +145,31 @@ export default function SocialShare() {
             viewBox="0 0 24 24"
           >
             <path d="m20.665 3.717-17.73 6.837c-1.21.486-1.203 1.161-.222 1.462l4.552 1.42 10.532-6.645c.498-.303.953-.14.579.192l-8.533 7.701h-.002l.002.001-.314 4.692c.46 0 .663-.211.921-.46l2.211-2.15 4.599 3.397c.848.467 1.457.227 1.668-.785l3.019-14.228c.309-1.239-.473-1.8-1.282-1.434z"></path>
+          </svg>
+        </div>
+
+        {/* SHARE ICON */}
+        <div
+          onClick={() => handleSocialShare("share")}
+          className="shadow-xl flex h-12 w-12 cursor-pointer items-center justify-center rounded-full border border-gray-200 hover:bg-gray-300"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="text-gray-500"
+          >
+            <circle cx="18" cy="5" r="3"></circle>
+            <circle cx="6" cy="12" r="3"></circle>
+            <circle cx="18" cy="19" r="3"></circle>
+            <line x1="8.59" y1="13.51" x2="15.42" y2="17.49"></line>
+            <line x1="15.41" y1="6.51" x2="8.59" y2="10.49"></line>
           </svg>
         </div>
       </div>
