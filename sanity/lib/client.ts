0c364452e739e4aa78fa015abe44b1e6c18b7f99
Mr. Hasan <hasanshahriar32@gmail.com>
2024-03-07 11:59:47 +0600

    update

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -7,7 +7,7 @@ export const client = createClient({
   apiVersion,
   dataset,
   projectId,
-  useCdn,
+  useCdn: true,
   // These settings will be overridden in
   // ./sanity/lib/store.ts when draftMode is enabled
   perspective: "published",
