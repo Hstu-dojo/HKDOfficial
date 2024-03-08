02f01537cbecd5d60abbf77fe43233f8a5528e01
MR. HASAN <hasanshahriar32@gmail.com>
2024-03-08 14:47:05 +0600

    update hall of frame

--- src/components/ui/container-scroll-animation.tsx
+++ src/components/ui/container-scroll-animation.tsx
@@ -40,11 +40,11 @@ export const ContainerScroll = ({
 
   return (
     <div
-      className="h-[80rem] flex items-center justify-center relative p-20"
+      className="h-[80rem] flex items-center justify-center relative"
       ref={containerRef}
     >
       <div
-        className="py-40 w-full relative"
+        className="py-20 w-full relative"
         style={{
           perspective: "1000px",
         }}
