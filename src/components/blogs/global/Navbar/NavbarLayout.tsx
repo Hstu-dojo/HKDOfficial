fa7eb4735038d0be572d93e2ed3543897925daf4
hasanshahriar32 <hasanshahriar32@gmail.com>
2024-09-01 18:49:24 +0600

    chore: Update EnrollForm layout and add HKD AI chat feature

--- src/components/blogs/global/Navbar/NavbarLayout.tsx
+++ src/components/blogs/global/Navbar/NavbarLayout.tsx
@@ -1,9 +1,10 @@
 import Link from "next/link";
-import { resolveHref } from "../../../../../sanity/lib/utils";
+
 import {
   MenuItem,
   SettingsPayload,
 } from "../../../../../sanity/lib/sanity_types";
+import PageSelection from "./PageSelection";
 // import { FloatingNavbar } from "./FloatingNav";
 interface NavbarProps {
   data: SettingsPayload;
@@ -13,27 +14,23 @@ export default function Navbar(props: NavbarProps) {
   const menuItems = data?.menuItems || ([] as MenuItem[]);
   // console.log(data);
   return (
-    <div className="sticky top-0 z-10 flex w-full flex-wrap items-center gap-x-5 bg-white/80 px-4 py-4 backdrop-blur md:px-16 md:py-5 lg:px-32">
-      {menuItems &&
-        menuItems.map((menuItem, key) => {
-          const href = resolveHref(menuItem?._type, menuItem?.slug);
-          if (!href) {
-            return null;
-          }
-          return (
-            <Link
-              key={key}
-              className={`text-lg hover:text-black md:text-xl ${
-                menuItem?._type === "home"
-                  ? "font-extrabold text-black"
-                  : "text-gray-600"
-              }`}
-              href={href === "/blog" ? `${href}` : `/blog${href}`}
-            >
-              {menuItem.title}
-            </Link>
-          );
-        })}
+    <div className="sticky justify-between top-0 z-10 flex w-full flex-wrap items-center gap-x-5 bg-white/80 px-4 py-4 backdrop-blur md:px-16 md:py-5 lg:px-32">
+      
+      <div className=''>
+        <Link
+          className='text-lg font-extrabold hover:text-black md:text-xl mr-3'
+          href='/blog'
+        >
+          HKD Blog
+        </Link>
+        <Link
+          className='text-lg text-gray-600 md:text-xl'
+          href='/'
+        >
+          Home
+        </Link>
+      </div>
+      <PageSelection menuItems={menuItems} />
       {/* <FloatingNavbar /> */}
     </div>
   );
---
+++ src/components/blogs/global/Navbar/PageSelection.tsx
@@ -0,0 +1,136 @@
+"use client"
+
+import * as React from "react"
+import { useRouter } from "next/navigation";
+import { useMediaQuery } from "@/hooks/use-media-query"
+import { Button } from "@/components/ui/button"
+import { resolveHref } from "../../../../../sanity/lib/utils";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "@/components/ui/command"
+import {
+  Drawer,
+  DrawerContent,
+  DrawerTrigger,
+} from "@/components/ui/drawer"
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover"
+
+type Status = {
+  value: string
+  label: string
+}
+
+// const statuses: Status[] = [
+//   {
+//     value: "backlog",
+//     label: "Backlog",
+//   },
+//   {
+//     value: "todo",
+//     label: "Todo",
+//   },
+//   {
+//     value: "in progress",
+//     label: "In Progress",
+//   },
+//   {
+//     value: "done",
+//     label: "Done",
+//   },
+//   {
+//     value: "canceled",
+//     label: "Canceled",
+//   },
+// ]
+
+export default function PageSelection({ menuItems }:any) {
+  const [open, setOpen] = React.useState(false);
+  const isDesktop = useMediaQuery("(min-width: 768px)");
+  const [selectedStatus, setSelectedStatus] = React.useState<Status | null>(
+    null,
+  );
+
+  if (isDesktop) {
+    return (
+      <Popover open={open} onOpenChange={setOpen}>
+        <PopoverTrigger asChild>
+          <Button variant="outline" className="w-[150px] justify-start">
+            {selectedStatus ? <>{selectedStatus.label}</> : <> Explore</>}
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className="w-[200px] p-0" align="start">
+          <StatusList menuItems={menuItems} setOpen={setOpen} setSelectedStatus={setSelectedStatus} />
+        </PopoverContent>
+      </Popover>
+    );
+  }
+
+  return (
+    <Drawer open={open} onOpenChange={setOpen}>
+      <DrawerTrigger asChild>
+        <Button variant="outline" className="w-[150px] justify-start">
+          {selectedStatus ? <>{selectedStatus.label}</> : <> Explore</>}
+        </Button>
+      </DrawerTrigger>
+      <DrawerContent>
+        <div className="mt-4 border-t">
+          <StatusList menuItems={menuItems} setOpen={setOpen} setSelectedStatus={setSelectedStatus} />
+        </div>
+      </DrawerContent>
+    </Drawer>
+  );
+}
+
+function StatusList({
+  setOpen,
+  setSelectedStatus,
+  menuItems
+}: {
+  setOpen: (open: boolean) => void
+  setSelectedStatus: (status: Status | null) => void
+  menuItems: any
+}) {
+  const router = useRouter();
+  return (
+    <Command>
+      <CommandInput placeholder="Filter status..." />
+      <CommandList>
+        <CommandEmpty>No results found.</CommandEmpty>
+        <CommandGroup>
+          {menuItems.map(
+            function(status: any){
+            const href = resolveHref(status?._type, status?.slug);
+            if (!href) {
+              return null;
+            }
+            return (
+              <CommandItem
+                key={status.title}
+                value={status.title}
+                onSelect={(value) => {
+                  setSelectedStatus(
+                    menuItems.find((priority: any) => priority.title === value) || null,
+                  );
+                  router.push(`/blog/${href}`);
+                  console.log(href)
+                  setOpen(false);
+                }}
+              >
+                {status.title}
+              </CommandItem>
+            );
+        })}
+        </CommandGroup>
+      </CommandList>
+    </Command>
+  );
+}
