69c17f79846850f1b80cce52af6f5b8b746e007e
hasanshahriar32 <hasanshahriar32@gmail.com>
2026-01-24 20:08:43 +0600

    feat: update session hook import to use custom compatibility hook

--- src/components/karate/CourseApplicationForm.tsx
+++ src/components/karate/CourseApplicationForm.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useState } from 'react';
-import { useSession } from 'next-auth/react';
+import { useSession } from '@/hooks/useSessionCompat';
 import { useRouter } from 'next/navigation';
 import Image from 'next/image';
 import Link from 'next/link';
