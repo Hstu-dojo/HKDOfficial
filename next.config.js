6b2e51251fbccbc5ac62918e3a4ad3a81ebb8dd5
hasanshahriar32 <hasanshahriar32@gmail.com>
2026-03-24 18:40:36 +0600

    updating ui issues

--- next.config.js
+++ next.config.js
@@ -11,6 +11,7 @@ const withPWA = require("@ducanh2912/next-pwa").default({
   reloadOnOnline: true,
   swcMinify: true,
   disable: process.env.NODE_ENV === "development",
+  buildExcludes: [/dynamic-css-manifest\.json$/],
   // disable: false,
   workboxOptions: {
     disableDevLogs: true,
